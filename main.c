402eaeabe0b34f5d209231ad48e597fa5580025d
certifiedkaren <kevinnxie1@gmail.com>
2026-02-19 20:00:05 -0500

    makes new mouse device now

--- main.c
+++ main.c
@@ -10,33 +10,21 @@ struct libevdev *dev;
 struct libevdev_uinput *uidev;
 
 int main(void) {
-  int fd, uifd, err;
-  // replace with mouse input file
-  fd = open("/dev/input/event14", O_RDONLY);
-  if (fd == -1) {
-    perror("failed to read file");
+  dev = libevdev_new();
+  libevdev_set_name(dev, "new mouse");
+  libevdev_enable_event_type(dev, EV_REL);
+  libevdev_enable_event_code(dev, EV_REL, REL_X, NULL);
+  libevdev_enable_event_code(dev, EV_REL, REL_Y, NULL);
+  libevdev_enable_event_type(dev, EV_KEY);
+  libevdev_enable_event_code(dev, EV_KEY, BTN_LEFT, NULL);
+
+  int new_dev = libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
+  if (new_dev != 0) {
+    perror("failed to create device");
     exit(EXIT_FAILURE);
   }
 
-  err = libevdev_new_from_fd(fd, &dev);
-  if (err != 0) {
-    perror("failed to initalize device");
-    exit(EXIT_FAILURE);
-  }
-
-  uifd = open("/dev/uinput", O_RDWR);
-  if (uifd == -1) {
-    perror("failed to read file");
-    exit(EXIT_FAILURE);
-  }
-
-  err = libevdev_uinput_create_from_device(dev, uifd, &uidev);
-  if (err != 0) {
-    perror("failed to initalize device");
-    exit(EXIT_FAILURE);
-  }
-
-  printf("mouse jiggler running\n");
+  printf("mouse wiggler running\n");
 
   while(1) {
     libevdev_uinput_write_event(uidev, EV_REL, REL_X, 4);
@@ -48,7 +36,5 @@ int main(void) {
 
   libevdev_uinput_destroy(uidev);
   libevdev_free(dev);
-  close(uifd);
-  close(fd);
   return 0;
 }
