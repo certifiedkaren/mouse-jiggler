3051d12461300a91c2b8ea9c1a4446fee7860756
certifiedkaren <kevinnxie1@gmail.com>
2026-05-14 10:42:40 -0400

    can press q or esc to exit

--- windows.c
+++ windows.c
@@ -5,10 +5,11 @@ mouse jiggler for windows
 
 #include <windows.h>
 #include <stdio.h>
+#include <conio.h>
 
 int main(void)
 {
-    printf("mouse jiggler running\n");
+    printf("mouse jiggler running. Press q or esc to quit\n");
 
     while (1)
     {
@@ -21,14 +22,27 @@ int main(void)
 
         SendInput(1, &input, sizeof(INPUT));
 
-        Sleep(1000);
+        Sleep(100);
 
         input.mi.dx = -4;
         input.mi.dy = 0;
         input.mi.dwFlags = MOUSEEVENTF_MOVE;
 
         SendInput(1, &input, sizeof(INPUT));
-        Sleep(10000);
+
+        for (int i = 0; i < 200; i++)
+        {
+            Sleep(50);
+            if (_kbhit())
+            {
+                int key = _getch();
+                if (key == 'q' || key == 'Q' || key == 27)
+                {
+                    printf("Exiting\n");
+                    return 0;
+                }
+            }
+        }
     }
     return 0;
 }
